92dbb4f1eb681d4ffab84887545138d667668e9e
Phumrapee Limpianchop <git@rayriffy.com>
2019-11-23 14:03:28 +0700

    :construction: Init Firebase Hosting properly

--- src/core/services/firebase.ts
+++ src/core/services/firebase.ts
@@ -1,16 +1,16 @@
-import firebase from 'firebase'
+import firebaseApp from 'firebase/app'
+import fetch from 'isomorphic-unfetch'
 
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+const fetchConfig = fetch('/__/firebase/init.json').then(r => r.json())
+
+const firebase = async () => {
+  if (!firebaseApp.apps.length) {
+    const config = await fetchConfig
+
+    return firebaseApp.initializeApp(config)
+  } else {
+    firebaseApp.app()
+  }
 }
 
-export default !firebase.apps.length
-  ? firebase.initializeApp(firebaseConfig)
-  : firebase.app()
+export default firebase
