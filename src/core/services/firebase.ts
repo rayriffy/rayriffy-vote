73913411ab4e688fcf9bf10b546fcb03b956911f
Phumrapee Limpianchop <git@rayriffy.com>
2019-11-23 14:59:07 +0700

    :fire: Move to axios for better types

--- src/core/services/firebase.ts
+++ src/core/services/firebase.ts
@@ -1,7 +1,20 @@
+import axios from 'axios'
 import firebaseApp from 'firebase/app'
-import fetch from 'isomorphic-unfetch'
 
-const fetchConfig = fetch('/__/firebase/init.json').then(r => r.json())
+interface IConfig {
+  apiKey: string
+  appId: string
+  authDomain: string
+  databaseURL: string
+  measurementId: string
+  messagingSenderId: string
+  projectId: string
+  storageBucket: string
+}
+
+const fetchConfig = axios
+  .get<IConfig>('/__/firebase/init.json')
+  .then(r => r.data)
 
 const firebase = async () => {
   if (!firebaseApp.apps.length) {
