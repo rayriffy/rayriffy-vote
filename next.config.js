903d5cc43de46212144b2eaf533fb9818c93c785
Phumrapee Limpianchop <git@rayriffy.com>
2020-01-21 22:57:19 +0700

    :zap: Optimization

--- next.config.js
+++ next.config.js
@@ -4,7 +4,36 @@ if (!isProduction) {
   require('dotenv').config()
 }
 
-module.exports = {
+const withPreact = (nextConfig = {}) => {
+	return Object.assign({}, nextConfig, {
+		webpack(config, options) {
+			if (!options.defaultLoaders) {
+				throw new Error(
+					"This plugin is not compatible with Next.js versions below 5.0.0 https://err.sh/next-plugins/upgrade"
+				)
+			}
+
+			if (options.isServer) {
+				config.externals = ["react", "react-dom", ...config.externals]
+			}
+
+			config.resolve.alias = Object.assign({}, config.resolve.alias, {
+				react: "preact/compat",
+				react$: "preact/compat",
+				"react-dom": "preact/compat",
+				"react-dom$": "preact/compat"
+			})
+
+			if (typeof nextConfig.webpack === "function") {
+				return nextConfig.webpack(config, options)
+			}
+
+			return config
+		}
+	})
+}
+
+module.exports = withPreact({
   env: {
     FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
     FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
@@ -15,4 +44,4 @@ module.exports = {
     FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
     FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID,
   },
-}
+})
